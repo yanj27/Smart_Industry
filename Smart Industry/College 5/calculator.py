679c7438f40c25c9da7c3adf1039c12985d4716b
Jiaming Yan <jiaming.yan@hva.nl>
2025-04-25 10:50:18 +0200

    College 5

---
+++ Smart Industry/College 5/calculator.py
@@ -0,0 +1,15 @@
+def calculate(operation, x, y):
+    if operation == 'Addition':
+        return x+y
+    
+    elif operation == 'Subtraction':
+        if x> y:
+            return x-y
+        else:
+            return y-x
+        
+    elif operation == 'Multiplication':
+        return x*y
+    
+    elif operation == 'Division':
+        return x/y
