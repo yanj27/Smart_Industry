81f9ca061bdfdb1955ac6c2ef57c77b9c45902e4
Jiaming <jiaming.yan@hva.nl>
2025-06-03 18:19:50 +0200

    Background subtraction

--- Smart Industry/College 9/motion_detection_utils.py
+++ Smart Industry/College 9/motion_detection_utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+from PIL import Image
 
 def get_contour_detections(mask, thresh=400):
     """ Obtains initial proposed detections from contours discoverd on the mask. 
@@ -22,3 +23,198 @@ def get_contour_detections(mask, thresh=400):
             detections.append([x,y,x+w,y+h, area])
 
     return np.array(detections)
+
+def compute_iou(box1, box2):
+    """ Obtains Intersection over union (IOU) of 2 bounding boxes
+        Inputs are in the form of:
+            xmin, ymin, xmax, ymax = box
+        """
+    x11, y11, x21, y21 = box1
+    x12, y12, x22, y22 = box2
+
+    # get box points of intersection
+    xi1 = max(x11, x12) # top left
+    yi1 = max(y11, y12)
+    xi2 = min(x21, x22) # bottom right
+    yi2 = min(y21, y22)
+
+    # compute intersectional area
+    inter_area = max((xi2 - xi1 + 1), 0) * max((yi2 - yi1 + 1), 0)
+    if inter_area == 0:
+        return inter_area
+
+    # compute box areas
+    box1_area = (x21 - x11 + 1) * (y21 - y11 + 1)
+    box2_area = (x22 - x12 + 1) * (y22 - y12 + 1)
+
+    # return iou
+    return inter_area / (box1_area + box2_area - inter_area)
+
+
+def get_inter_area(box1, box2):
+    """
+    Obtains bounding box for intersection area of two boundning boxes
+    Inputs are in the form of:
+            xmin, ymin, xmax, ymax = box
+    """
+    x11, y11, x21, y21 = box1
+    x12, y12, x22, y22 = box2
+
+    # get box points of intersection
+    xi1 = max(x11, x12) # top left
+    yi1 = max(y11, y12)
+    xi2 = min(x21, x22) # bottom right
+    yi2 = min(y21, y22)
+
+    # compute intersectional area
+    inter_area = max((xi2 - xi1 + 1), 0) * max((yi2 - yi1 + 1), 0)
+    if inter_area == 0:
+        return 0, 0, 0, 0
+        
+    return xi1, yi1, xi2, yi2
+
+def remove_contained_bboxes(boxes):
+    """ Removes all smaller boxes that are contained within larger boxes.
+        Requires bboxes to be soirted by area (score)
+        Inputs:
+            boxes - array bounding boxes sorted (descending) by area 
+                    [[x1,y1,x2,y2]]
+        Outputs:
+            keep - indexes of bounding boxes that are not entirely contained 
+                   in another box
+        """
+    check_array = np.array([True, True, False, False])
+    keep = list(range(0, len(boxes)))
+    for i in keep: # range(0, len(bboxes)):
+        for j in range(0, len(boxes)):
+            # check if box j is completely contained in box i
+            if np.all((np.array(boxes[j]) >= np.array(boxes[i])) == check_array):
+                try:
+                    keep.remove(j)
+                except ValueError:
+                    continue
+    return keep
+
+
+def non_max_suppression(boxes, scores, threshold=1e-1):
+    """
+    Perform non-max suppression on a set of bounding boxes and corresponding scores.
+    Inputs:
+        boxes: a list of bounding boxes in the format [xmin, ymin, xmax, ymax]
+        scores: a list of corresponding scores 
+        threshold: the IoU (intersection-over-union) threshold for merging bounding boxes
+    Outputs:
+        boxes - non-max suppressed boxes
+    """
+    # Sort the boxes by score in descending order
+    boxes = boxes[np.argsort(scores)[::-1]]
+
+    # remove all contained bounding boxes and get ordered index
+    order = remove_contained_bboxes(boxes)
+
+    keep = []
+    while order:
+        i = order.pop(0)
+        keep.append(i)
+        for j in order:
+            # Calculate the IoU between the two boxes
+            intersection = max(0, min(boxes[i][2], boxes[j][2]) - max(boxes[i][0], boxes[j][0])) * \
+                           max(0, min(boxes[i][3], boxes[j][3]) - max(boxes[i][1], boxes[j][1]))
+            union = (boxes[i][2] - boxes[i][0]) * (boxes[i][3] - boxes[i][1]) + \
+                    (boxes[j][2] - boxes[j][0]) * (boxes[j][3] - boxes[j][1]) - intersection
+            iou = intersection / union
+
+            # Remove boxes with IoU greater than the threshold
+            if iou > threshold:
+                order.remove(j)
+                
+    return boxes[keep]
+
+def non_max_suppression_2(boxes, scores, threshold=1e-1):
+    """
+    Perform non-max suppression on a set of bounding boxes and corresponding scores.
+    NOTE: Eventhough we only go through 2 loops here, this way is more complicated and slower!
+    Inputs:
+        boxes: a list of bounding boxes in the format [xmin, ymin, xmax, ymax]
+        scores: a list of corresponding scores 
+        threshold: the IoU (intersection-over-union) threshold for merging bounding boxes
+    Outputs:
+        boxes - non-max suppressed boxes
+    """
+    # Sort the boxes by score in descending order
+    boxes = boxes[np.argsort(scores)[::-1]]
+
+    keep = list(range(0, len(boxes)))
+    for i in keep:
+        for j in range(0, len(bboxes)):
+            # check if box j is completely contained in box i
+            if np.all((np.array(boxes[j]) >= np.array(boxes[i])) == np.array([True, True, False, False])):
+                try:
+                    keep.remove(j)
+                except ValueError:
+                    continue
+            # if no overlap check IOU threshold
+            else:
+                # Calculate the IoU between the two boxes
+                intersection = max(0, min(boxes[i][2], boxes[j][2]) - max(boxes[i][0], boxes[j][0])) * \
+                            max(0, min(boxes[i][3], boxes[j][3]) - max(boxes[i][1], boxes[j][1]))
+                union = (boxes[i][2] - boxes[i][0]) * (boxes[i][3] - boxes[i][1]) + \
+                        (boxes[j][2] - boxes[j][0]) * (boxes[j][3] - boxes[j][1]) - intersection
+                iou = intersection / union
+
+                # Remove boxes with IoU greater than the threshold
+                # ensure that we don't remove larger boxes by checking (j > i)
+                if (iou > threshold) and (j > i):
+                    try:
+                        keep.remove(j)
+                    except ValueError:
+                        continue
+    return boxes[keep]
+
+def get_detections(frame1, frame2, bbox_thresh=400, nms_thresh=1e-3, mask_kernel=np.array((9,9), dtype=np.uint8)):
+    """ Main function to get detections via Frame Differencing
+        Inputs:
+            frame1 - Grayscale frame at time t
+            frame2 - Grayscale frame at time t + 1
+            bbox_thresh - Minimum threshold area for declaring a bounding box 
+            nms_thresh - IOU threshold for computing Non-Maximal Supression
+            mask_kernel - kernel for morphological operations on motion mask
+        Outputs:
+            detections - list with bounding box locations of all detections
+                bounding boxes are in the form of: (xmin, ymin, xmax, ymax)
+        """
+    # get image mask for moving pixels
+    mask = get_mask(frame1, frame2, mask_kernel)
+
+    # get initially proposed detections from contours
+    detections = get_contour_detections(mask, bbox_thresh)
+
+    # separate bboxes and scores
+    bboxes = detections[:, :4]
+    scores = detections[:, -1]
+
+    # perform Non-Maximal Supression on initial detections
+    return non_max_suppression(bboxes, scores, nms_thresh)
+
+def draw_bboxes(frame, detections):
+    for det in detections:
+        x1,y1,x2,y2 = det
+        cv2.rectangle(frame, (x1,y1), (x2,y2), (0,255,0), 3)
+
+
+def create_gif_from_images(save_path : str, image_path : str, ext : str) -> None:
+    ''' creates a GIF from a folder of images
+        Inputs:
+            save_path - path to save GIF
+            image_path - path where images are located
+            ext - extension of the images
+        Outputs:
+            None
+    '''
+    ext = ext.replace('.', '')
+    image_paths = sorted(glob(os.path.join(image_path, f'*.{ext}')))
+    image_paths.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
+    pil_images = [Image.open(im_path) for im_path in image_paths]
+
+    pil_images[0].save(save_path, format='GIF', append_images=pil_images,
+                       save_all=True, duration=50, loop=0)
