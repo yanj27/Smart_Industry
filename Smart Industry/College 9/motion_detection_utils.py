3c39c09a5551bf3497de79a400af724a351531e6
Jiaming <jiaming.yan@hva.nl>
2025-06-03 18:06:30 +0200

    Motion Detection

---
+++ Smart Industry/College 9/motion_detection_utils.py
@@ -0,0 +1,24 @@
+import numpy as np
+import cv2
+
+def get_contour_detections(mask, thresh=400):
+    """ Obtains initial proposed detections from contours discoverd on the mask. 
+        Scores are taken as the bbox area, larger is higher.
+        Inputs:
+            mask - thresholded image mask
+            thresh - threshold for contour size
+        Outputs:
+            detectons - array of proposed detection bounding boxes and scores [[x1,y1,x2,y2,s]]
+        """
+    # get mask contours
+    contours, _ = cv2.findContours(mask, 
+                                   cv2.RETR_EXTERNAL, # cv2.RETR_TREE, 
+                                   cv2.CHAIN_APPROX_TC89_L1)
+    detections = []
+    for cnt in contours:
+        x,y,w,h = cv2.boundingRect(cnt)
+        area = w*h
+        if area > thresh: # hyperparameter
+            detections.append([x,y,x+w,y+h, area])
+
+    return np.array(detections)
